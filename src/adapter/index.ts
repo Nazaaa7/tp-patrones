1bc06d15d280df3cd630840cf4f387e4ac93cbf3
Naza Garcia <nazagarcia132@gmail.com>
2025-09-09 12:48:06 -0300

    se terminaron los patrones

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -4,11 +4,11 @@ import { SimpleHttpClient } from "./clases/SimpleHttpClient";
 export async function runAdapter() {
   const client = new SimpleHttpClient(new FetchAdapter());
 
-  console.log("Usando FetchAdapter:");
+  console.log("FetchAdapter:");
   const posts1 = await client.get<any[]>("https://jsonplaceholder.typicode.com/posts?_limit=2");
   console.log(posts1);
 
-  console.log(" Cambiando a AxiosAdapter:");
+  console.log(" AxiosAdapter:");
   client.setAdapter(new AxiosAdapter());
   const posts2 = await client.get<any[]>("https://jsonplaceholder.typicode.com/posts?_limit=2");
   console.log(posts2);
